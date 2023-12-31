50b1d3da1ba7e1d9ccac571984202fd6f6c0f530
Swarup Mahato <115489346+swarupinfotech@users.noreply.github.com>
2023-12-31 15:19:05 +0530

    swarupinfotech-official

---
+++ data.js
@@ -0,0 +1,14 @@
+const data_name = [
+    {
+        ma_ten: "Swarup",
+        name: "Swarup",
+        message: "Hello, I'm Swarup",
+        img: "https://vcdn1-giaitri.vnecdn.net/2014/11/12/12-1415753927.jpg?w=1200&h=0&q=100&dpr=1&fit=crop&s=eeNdzX-KP7izXlmIm_xMCg"
+    },
+    {
+        ma_ten: "Swarup Kumar",
+        name: "Swarup Kumar",
+        message: "Hello, I'm Swarup Kumar",
+        img: "https://vcdn1-giaitri.vnecdn.net/2014/11/12/12-1415753927.jpg?w=1200&h=0&q=100&dpr=1&fit=crop&s=eeNdzX-KP7izXlmIm_xMCg"
+    }
+]
